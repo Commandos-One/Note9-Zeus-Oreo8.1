ceb09ccf82e5d80dd46a36a65f7325b48587df6f
THEBOSS619 <ehabhani59@gmail.com>
2019-05-08 16:03:24 +0300

    Improve CPU idle

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -626,7 +626,17 @@ EXPORT_SYMBOL_GPL(cpuidle_register);
 static int cpuidle_latency_notify(struct notifier_block *b,
 		unsigned long l, void *v)
 {
-	wake_up_all_idle_cpus();
+	static unsigned long prev_latency = ULONG_MAX;
+	struct cpumask cpus;
+	
+	if (l < prev_latency) {
+		cpumask_andnot(&cpus, cpu_online_mask, cpu_isolated_mask);
+		preempt_disable();
+		smp_call_function_many(&cpus, smp_callback, NULL, false);
+		preempt_enable();
+	}
+	prev_latency = l;
+	
 	return NOTIFY_OK;
 }
 
